15dbddcab708faa5b8135663588b36d97fc621f6
yuvrajinbhakti <yuvrajsinghnain03@gmail.com>
2024-10-10 20:26:51 +0530

    Fixed bugs

---
+++ index.js
@@ -0,0 +1,4 @@
+// index.js
+import { getOriginalUrl } from "./src/simplestUrlShortner.js";
+import { shortenUrl } from "./src/simplestUrlShortner.js";
+export { shortenUrl, getOriginalUrl };
--- src/index.js
+++
@@ -1,4 +0,0 @@
-// index.js
-import {getOriginalUrl} from "./simplestUrlShortner.js";
-import {shortenUrl} from "./simplestUrlShortner.js";
-export { shortenUrl, getOriginalUrl };
